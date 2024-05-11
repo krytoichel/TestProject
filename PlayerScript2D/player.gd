baa3873678804c6ec3c4df975ca44cb93146dfe1
IAntonPV <150920224+IAntonPV@users.noreply.github.com>
2024-05-11 20:46:29 +0300

    State machine2

--- PlayerScript2D/player.gd
+++ PlayerScript2D/player.gd
@@ -4,15 +4,14 @@ extends CharacterBody2D
 enum {
 	ATTACK_RUB,
 	ATTACK_KOL,
+	ATTACK_RUB1,
+	ATTACK_KOL1,
 	CLIMB,
-	DEATH,
-	FALL,
 	HIT,
-	JUMP,
 	KICK,
 	ROLL,
 	SHIELD,
-	RUN
+	RUN,
 }
 
 #переменные
@@ -30,32 +29,33 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var health = 100
 var shard = 0
 var state = RUN
+var combo1 = false
+var combo2 = false
+var attack_cooldown = false
 
 #процессы, происходящие на левеле постоянно
 func _physics_process(delta):
 	match state:
 		ATTACK_RUB:
-			move_state()
+			attack1_state()#1
 		ATTACK_KOL:
-			move_state()
+			attack2_state()#1
+		ATTACK_RUB1:
+			attack11_state()#1
+		ATTACK_KOL1:	
+			attack22_state()#1
 		CLIMB:
-			move_state()
-		DEATH:
-			move_state()
-		FALL:
 			move_state()
 		HIT:
 			move_state()
-		JUMP:
-			move_state()
 		KICK:
-			move_state()
+			kick_state()#1
 		ROLL:
-			move_state()
+			roll_state()#1
 		SHIELD:
-			move_state()
+			shield_state()#1
 		RUN:
-			move_state()
+			move_state()#1
 			
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -98,10 +98,92 @@ func move_state():
 			velocity.y = JUMP_VELOCITY
 			animPlayer.play('Jump')
 
-		#атака
-		if Input.is_action_just_pressed('attack_rub'):
+		#блок
+		if Input.is_action_pressed('shield') and velocity.y == 0:
+			state = SHIELD
+		
+		#кувырок
+		if Input.is_action_just_pressed('roll') and velocity.x != 0 and velocity.y == 0:
+			state = ROLL
+			
+		#атака1
+		if Input.is_action_just_pressed('attack_rub') and attack_cooldown == false:
 			state = ATTACK_RUB
+			
+		#атака2
+		if Input.is_action_just_pressed('attack_kol') and attack_cooldown == false:
+			state = ATTACK_KOL
+		
+		#кик
+		if Input.is_action_just_pressed('kick'):
+			state = KICK
+			
+func shield_state():
+	velocity.x = 0
+	animPlayer.play('Shield_active', -1, 1.0, true)
+	if Input.is_action_just_released('shield'):
+		state = RUN
+	
+func roll_state():
+	animPlayer.play('Roll')
+	await animPlayer.animation_finished
+	state = RUN
 
+func attack1_state():
+	if Input.is_action_just_pressed('attack_rub') and combo1 == true:
+		state = ATTACK_RUB1
+	velocity.x = 0
+	animPlayer.play('Attack4')
+	await animPlayer.animation_finished
+	attack_freeze()
+	state = RUN
+
+func attack11_state():
+	animPlayer.play('Attack1')
+	await animPlayer.animation_finished
+	attack_freeze()
+	state = RUN
+
+func attack2_state():
+	if Input.is_action_just_pressed('attack_kol') and combo2 == true:
+		state = ATTACK_KOL1
+	velocity.x = 0
+	animPlayer.play('Attack2')
+	await animPlayer.animation_finished
+	attack_freeze()
+	state = RUN
+
+func attack22_state():
+	animPlayer.play('Attack3')
+	await animPlayer.animation_finished
+	attack_freeze()
+	state = RUN
+
+func combo1_state():
+	combo1 = true
+	await animPlayer.animation_finished
+	combo1 = false
+
+func combo2_state():
+	combo2 = true
+	await animPlayer.animation_finished
+	combo1 = false
+
+func kick_state():
+	velocity.x = 0
+	animPlayer.play('Kick')
+	await animPlayer.animation_finished
+	state = RUN
+
+func attack_freeze():
+	attack_cooldown = true
+	await get_tree().create_timer(0.1).timeout
+	attack_cooldown = false
+	
+	
+
+
+	
 	
 	
 
