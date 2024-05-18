a8c9bdb55963ed1cc0bd9a236a364a2c23f6ff96
admin1393 <abdyrrakhman.kimpaev@gmail.com>
2024-05-18 21:45:33 +0300

    Revert "mob's"
    This reverts commit f6e83b93460a8f660b9c405dea961c27664b6b8b.

--- MobsScript2D/Goblin1.gd
+++ MobsScript2D/Goblin1.gd
@@ -3,14 +3,12 @@ extends CharacterBody2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var chase = false
-var chase1 = false
-
 
 var speed = 100
 
-@onready var anim = $AnimatedSprite2D 
-var alive = true
+@onready var anim = $AnimatedSprite2D
 
+var alive = true
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,31 +17,12 @@ func _physics_process(delta):
 	var player = $"../../Player/Player"
 	var direction = (player.position - self.position).normalized()
 	if alive == true:
-		if chase == true and chase1 == false:
+		if chase == true:
 			velocity.x = direction.x * speed
 			anim.play('run')
-			if abs(player.position.x - self.position.x) < 30:
-				anim.play("idle")
-		#elif chase == false and chase1 == true:
-			#velocity.x = 0
-			##anim.play("Attack")
-			##await anim.animation_finished
-		
-			
-		elif chase == false and chase1 == false:
+		else:
 			velocity.x = 0
 			anim.play('idle')
-			
-		#if chase1 == true:
-			#chase = false
-			#anim.play("")
-			#anim.stop()
-			#await anim.animation_finished
-			#anim.play("Attack")
-		#else:
-			#chase = false
-			
-		
 		if direction.x < 0:
 			$AnimatedSprite2D.flip_h = true
 		else:
@@ -53,60 +32,30 @@ func _physics_process(delta):
 func _on_detector_body_entered(body):
 	if body.name == 'Player':
 		chase = true
-		chase1 = false
 
 
 func _on_detector_body_exited(body):
 	if body.name == 'Player':
 		chase = false
-		chase1 = false
 
 
 func _on_death_body_entered(body):
 	if body.name == 'Player':
 		body.velocity.y -= 200
 		death()
-#
-#func _on_death_2_body_entered(body):
-	#if body.name == 'Player':
-		##if alive == true:
-			##body.health -= 40
-			##death()
-#
+
+func _on_death_2_body_entered(body):
+	if body.name == 'Player':
+		if alive == true:
+			body.health -= 40
+			death()
+
 func death():
 	alive = false
 	anim.play('death')
 	await anim.animation_finished
 	queue_free()
 	
-
-
-
-
-
-func _on_detector_atack_body_entered(body):
-	if body.name == 'Player':
-		chase1 = true
-		chase = false
-		anim.play("Attack")
-		await get_tree().create_timer(0.8).timeout
-		body.anim.play("Hit_idle_B")
-		body.health -= 20
-		var det = $DetectorAtack/CollisionShape2D
-		det.disabled = 1
-		await get_tree().create_timer(1).timeout
-		det.disabled = 0
-		anim.speed_scale = 1
-		#await get_tree().create_timer(1).timeout
-
-
-func _on_detector_atack_body_exited(body):
-	if body.name == 'Player':
-		chase1 = false
-		chase = true
-	
-
-
 
 
 
