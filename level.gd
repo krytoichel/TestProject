5b920d97474f3b55d14fcade88499fd9b8040058
admin1393 <abdyrrakhman.kimpaev@gmail.com>
2024-05-18 21:51:55 +0300

    add mob's

--- level.gd
+++ level.gd
@@ -2,36 +2,8 @@ extends Node2D
 
 @onready var light = $DirectionalLight2D
 
-enum {
-	MORNING,
-	DAY,
-	EVENING,
-	NIGHT
-}
-
-var state = MORNING
-
-func _ready():
-	light.enabled = true
-	
 func _process(_delta):
-	match state:
-		MORNING:
-			morning_state()
-		EVENING:
-			evening_state()
-			
-func morning_state ():
 	var tween = get_tree().create_tween()
-	tween.tween_property(light, "energy", 0.2, 20)
-func evening_state ():
-	var tween = get_tree().create_tween()
-	tween.tween_property(light, "energy", 0.9, 20)
-
-
-
-func _on_gay_night_timeout():
-	if state < 3:
-		state += 1
-	else:
-		state = MORNING
+	tween.tween_property(light, 'energy', 0.2, 5)
+		
+	
